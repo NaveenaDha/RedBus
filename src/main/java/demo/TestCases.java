fc28cd0fae739442b8f6d23211669a3806383df8
Naveena Andril <naveena.rajeei@gmail.com>
2023-09-01 22:17:27 +0530

    REdBus added

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,16 +1,14 @@
 package demo;
-
-import java.util.concurrent.TimeUnit;
+// import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 //Selenium Imports
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.remote.BrowserType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.Select;
-///
-
+// import org.openqa.selenium.Keys;
+// import org.openqa.selenium.remote.BrowserType;
+// import org.openqa.selenium.remote.DesiredCapabilities;
+// import org.openqa.selenium.remote.RemoteWebDriver;
+// import org.openqa.selenium.support.ui.Select;
 
 public class TestCases {
     ChromeDriver driver;
@@ -20,7 +18,7 @@ public class TestCases {
         WebDriverManager.chromedriver().timeout(30).setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
     }
 
@@ -31,13 +29,10 @@ public class TestCases {
         driver.quit();
 
     }
-
     
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
         driver.get("https://www.google.com");
         System.out.println("end Test case: testCase02");
     }
-
-
 }
